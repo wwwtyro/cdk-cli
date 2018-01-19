85346829effb9f1f3f6bf2e729a213ffd4f3f9df
Rye Terrell <ryeterrell@ryeterrell.net>
2018-01-19 15:03:39 -0600

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='cdk',
+    version='0.1',
+    description='Canonical Distribution of Kubernetes management utility',
+    scripts=['scripts/cdk']
+)
